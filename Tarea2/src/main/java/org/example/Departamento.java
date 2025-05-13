e0a1c7f0d59547037051d2909f4c63bf0646545c
pandita45 <jacastillo2024@udec.cl>
2025-05-13 18:26:30 -0400

    Documentacion de Empleado y Departamento

--- Tarea2/src/main/java/org/example/Departamento.java
+++ Tarea2/src/main/java/org/example/Departamento.java
@@ -4,25 +4,52 @@ import java.util.ArrayList;
 public class Departamento implements Invitable{
     private String nombre;
     protected ArrayList<Empleado> empleados;
+
+    /** Constructor de Departamento
+     *
+     * @param nombre es un String con el nombre del departamento
+     */
     public Departamento(String nombre){
         this.nombre = nombre;
         this.empleados = new ArrayList<>();
     }
 
+    /** Getter de nombre del departamento
+     *
+     * @return String con el nombre del departamento
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /** Setter del nombre del departamento
+     *
+     * @param newNombre String con el nuevo nombre del departamento
+     */
     public void setNombre(String newNombre){
         this.nombre = newNombre;
     }
+
+    /** Metodo para agregar empleado al departamento
+     *
+     * @param a es un empleado
+     */
     public void addEmpleado(Empleado a){
         empleados.add(a);
     }
+
+    /** Metodo para obtener cantidad de empleados
+     *
+     * @return un int con la cantidad de empleados
+     */
     public int obtenerCantidadEmpleados(){
         return empleados.size();
     }
 
+    /** Metodo para invitar un departamento a una reunion
+     *
+     * @param a es una reunion
+     */
     @Override
     public void invitar(Reunion a) {
         for(Empleado empleados : empleados){
@@ -30,6 +57,10 @@ public class Departamento implements Invitable{
         }
     }
 
+    /**Metodo toString
+     *
+     * @return String con los datos del departamento
+     */
     @Override
     public String toString() {
         return "Departamento " +nombre + ", empleados=" + empleados;
--- Tarea2/src/main/java/org/example/Empleado.java
+++ Tarea2/src/main/java/org/example/Empleado.java
@@ -6,30 +6,65 @@ public class Empleado implements Invitable {
     private String Nombre;
     private String Correo;
 
+    /** Constructor de la clase empleado
+     * 
+     * @param id inicializa el id del empleado
+     * @param nombre inicializa el nombre del empleado
+     * @param apellidos inicializa los apellidos del empleado
+     * @param correo inicializa el correo del empleado
+     */
     public Empleado(String id, String nombre, String apellidos, String correo){
         this.id = id;
         this.Apellidos = apellidos;
         this.Nombre = nombre;
         this.Correo = correo;
     }
+
+    /** Metodo getter de id
+     * 
+     * @return String con el id del empleado
+     */
     public String getId(){
         return id;
     }
+
+    /** Metodo getter de los apellidos
+     * 
+     * @return String con los apellidos del empleado
+     */
     public String getApellidos(){
         return Apellidos;
     }
+
+    /** Metodo getter del nombre
+     * 
+     * @return String con el nombre del empleado
+     */
     public String getNombre(){
         return Nombre;
     }
+
+    /** Metodo getter de correo
+     * 
+     * @return el String con el correo del empleado
+     */
     public String getCorreo(){
         return Correo;
     }
 
+    /** Metodo para invitar a una reunion
+     *
+     * @param a es la reunion a la que se invitara al empleado
+     */
     @Override
     public void invitar(Reunion a) {
         a.invitarAReunion(this);
     }
 
+    /** Metodo ToString
+     *
+     * @return String con todos los datos del empleado
+     */
     @Override
     public String toString() {
         return "Empleado{" + "id='" + id + '\'' + ", Apellidos='" + Apellidos + '\'' + ", Nombre='" + Nombre + '\'' + ", Correo='" + Correo + '\'' + '}';
